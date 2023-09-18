0d51685ec10cfc42ee04a749a5749b67a6782dd3
Corentin Braud <corentin.braud95@gmail.com>
2023-09-18 16:08:42 +0200

    Modification multer-config et ajout sharp middleware pour optimisation des images

---
+++ back-end/middleware/sharp.js
@@ -0,0 +1,26 @@
+const sharp = require("sharp")
+const fs = require("fs")
+
+module.exports = async (req, res, next) => {
+  if (!req.file) {
+    return next()
+  }
+  try {
+    const originalFilePath = req.file.path
+
+    const modifiedImageBuffer = await sharp(originalFilePath)
+      .resize({ height: 600 })
+      .jpeg({ quality: 90 })
+      .toBuffer()
+
+    // Supprimez l'image d'origine
+    fs.unlinkSync(originalFilePath)
+
+    // Enregistrez la nouvelle image modifiée
+    fs.writeFileSync(originalFilePath, modifiedImageBuffer)
+
+    next()
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+}
--- back-end/routes/book.js
+++ back-end/routes/book.js
@@ -4,13 +4,14 @@ const router = express.Router()
 const auth = require("../middleware/auth")
 const bookCtrl = require("../controllers/book")
 const multer = require("../middleware/multer-config")
+const sharp = require("../middleware/sharp")
 
 router.get("/", bookCtrl.getAllBooks)
 router.get("/bestrating", bookCtrl.getBestRatingBooks)
 router.get("/:id", bookCtrl.getOneBook)
-router.post("/", auth, multer, bookCtrl.createBook)
+router.post("/", auth, multer, sharp, bookCtrl.createBook)
 router.post("/:id/rating", auth, bookCtrl.rateBook)
-router.put("/:id", auth, multer, bookCtrl.modifyBook)
+router.put("/:id", auth, multer, sharp, bookCtrl.modifyBook)
 router.delete("/:id", auth, bookCtrl.deleteBook)
 
 module.exports = router
